37f91a06c7c7cbc5631745a45c456df3ef93a7d1
Justin Yang <justinyang@Justins-MacBook-Pro.local>
2023-04-18 02:47:29 -0400

    Display

--- rangeFinder.c
+++ rangeFinder.c
@@ -187,7 +187,92 @@ void bno055() {
     get_i2cbus(i2c_bus, senaddr);
     print_calstat();
     struct bnoinf bnoi;
-    res = get_inf(&bnoi);
+    int res = get_inf(&bnoi);
+    /* ----------------------------------------------------------- *
+       * print the formatted output strings to stdout                *
+       * ----------------------------------------------------------- */
+    printf("\nBN0055 Information at %s", ctime(&tsnow));
+    printf("----------------------------------------------\n");
+    printf("   Chip Version ID = 0x%02X\n", bnoi.chip_id);
+    printf("  Accelerometer ID = 0x%02X\n", bnoi.acc_id);
+    printf("      Gyroscope ID = 0x%02X\n", bnoi.gyr_id);
+    printf("   Magnetoscope ID = 0x%02X\n", bnoi.mag_id);
+    printf("  Software Version = %d.%d\n", bnoi.sw_msb, bnoi.sw_lsb);
+    printf("   Operations Mode = "); print_mode(bnoi.opr_mode);
+    printf("        Power Mode = "); print_power(bnoi.pwr_mode);
+    printf("Axis Configuration = "); print_remap_conf(bnoi.axr_conf);
+    printf("   Axis Remap Sign = "); print_remap_sign(bnoi.axr_sign);
+    printf("System Status Code = "); print_sstat(bnoi.sys_stat);
+    printf("System Clocksource = "); print_clksrc();
+
+    printf("Accelerometer Test = ");
+    if((bnoi.selftest >> 0) & 0x01) printf("OK\n");
+    else printf("FAIL\n");
+
+    printf(" Magnetometer Test = ");
+    if((bnoi.selftest >> 1) & 0x01) printf("OK\n");
+    else printf("FAIL\n");
+
+    printf("    Gyroscope Test = ");
+    if((bnoi.selftest >> 2) & 0x01) printf("OK\n");
+    else printf("FAIL\n");
+
+    printf("MCU Cortex M0 Test = ");
+    if((bnoi.selftest >> 3) & 0x01) printf("OK\n");
+    else printf("FAIL\n");
+
+    printf(" System Error Code = ");
+    switch(bnoi.sys_err) {
+        case 0x00:
+            printf("No Error\n");
+            break;
+        case 0x01:
+            printf("Peripheral initialization error\n");
+            break;
+        case 0x02:
+            printf("System initializion error\n");
+            break;
+        case 0x03:
+            printf("Selftest result failed\n");
+            break;
+        case 0x04:
+            printf("Register map value out of range\n");
+            break;
+        case 0x05:
+            printf("Register map address out of range\n");
+            break;
+        case 0x06:
+            printf("Register map write error\n");
+            break;
+        case 0x07:
+            printf("BNO low power mode not available\n");
+            break;
+        case 0x08:
+            printf("Accelerometer power mode not available\n");
+            break;
+        case 0x09:
+            printf("Fusion algorithm configuration error\n");
+            break;
+        case 0x0A:
+            printf("Sensor configuration error\n");
+            break;
+    }
+
+    print_unit(bnoi.unitsel);
+
+    printf("Sensor Temperature = ");
+    if(bnoi.opr_mode > 0) {
+        if((bnoi.unitsel >> 4) & 0x01) printf("%d°F\n", bnoi.temp_val);
+        else printf("%d°C\n",bnoi.temp_val);
+    }
+    else  printf("no data in CONFIG mode\n");
+
+    printf("\n----------------------------------------------\n");
+    struct bnoaconf bnoac;
+    if(get_acc_conf(&bnoac) == 0) print_acc_conf(&bnoac);
+
+    printf("\n----------------------------------------------\n");
+    print_calstat();
 }
 
 
