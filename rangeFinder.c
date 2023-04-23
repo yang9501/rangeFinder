082ac32f5e857012fd9ca7d216183c107bf8dabf
Justin Yang <justinyang@Justins-MacBook-Pro.local>
2023-04-23 19:37:21 -0400

    IMU testing

--- rangeFinder.c
+++ rangeFinder.c
@@ -220,7 +220,7 @@ void tiltCompensatedCompass() {
         psi = fmod(((360*atan2(Ym, Xm))/(2*M_PI)) + 360, 360);  //HEADING IN DEGREES
         printf("DEGREES HEADING: %f\n", psi);
 
-        usleep(1000 * 1000); //Sleep for 10 milliseconds
+        usleep(10 * 1000); //Sleep for 10 milliseconds
     }
 }
 
