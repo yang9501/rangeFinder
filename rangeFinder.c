526fe1aeb77f87b167de3dbe8eb913d343fe4f91
Justin Yang <justinyang@Justins-MacBook-Pro.local>
2023-04-24 23:45:35 -0400

    Refactoring and added tests

--- rangeFinder.c
+++ rangeFinder.c
@@ -13,9 +13,28 @@
 #include "SSD1306-OLED-display-driver-for-BeagleBone-master/I2C_Library/I2C.h"
 #include "pi-bno055-master/getbno055.h"
 #include "unitTests.h"
-#include "rangefinder.h"
 
 #define GPIO_PATH_66 "/sys/class/gpio/gpio66" //Start/Stop Button
+
+//Writes specified value to specified GPIO directory
+static void writeGPIO(char *filename, char *port, char *value);
+//Reads input to GPIO pin
+static uint32_t readGPIO(char *filename, char *port);
+//Primary button press detection
+void getButtonPress(void *buttonPort);
+void printCalibrationDisplay();
+//GPS functions
+double degreesToDecimal(double degreeCoord);                                                                                 ///TESTABLE
+void newCoords(double initLat, double initLong, double dx, double dy, double* targetLat, double* targetLong);                ///TESTABLE
+void parseGPSMessage(char* message, double* latResult, double* longResult);                                                                                         ///TESTABLE
+void readGPS();
+//Rangefinder function
+void rangeFinder();
+//Compass functions
+void getCalStatus();
+void polarToCartesianCoords(double r, double theta, double* x, double* y);                                                   ///TESTABLE
+void bno055();
+void tiltCompensatedCompass();
 
 ///////DATA VARIABLES
 //Calibration variables
